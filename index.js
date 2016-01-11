fedb868736d7fd000662f7d79c53289c8998c5f4
Denys Zaliskyi <deniszalessky@gmail.com>
2016-01-11 16:42:34 +0100

    stream based dir reader

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var reader = require('./lib/read_xlsx');
+// var Reader = require('./lib/read_xlsx');
+// var r = new Reader('./test/doublers');
+//
+// r.on('data', function(data){
+//     console.log(data);
+//   }).on('end', function(){
+//     console.log('The end is here');
+//   });
 
-console.log(reader('./test/doublers'));
+var r = require('./lib/read_xlsx')
+r('./test/doublers')
+
+//
+// var fsReaddir = require('fs-readdir');
+//
+// fsReaddir('./test/doublers')
+//   .on('file', function(obj){
+//     console.log('-----');
+//     console.log(obj)})
+//   .on('finish', function(obj){
+//     console.log('+++++');
+//     console.log(obj);
+//   })
