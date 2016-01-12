2c37f9b018b91ae1ffe821f5ce22c2a9514bd4ba
Denys Zaliskyi <deniszalessky@gmail.com>
2016-01-12 12:32:16 +0100

    some commit

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var Reader = require('./lib/read_xlsx');
 
 var r  = new Reader('./test/doublers');
 
+// r.pipe(process.stdOut);
 r.on('readable', function() {
   console.log(r.read());
-})
+});
+//
+// r.on('error', function(err){
+//   console.log(err);
+// });
+
+// var funks = require('./lib/read_xlsx')
+// funks.getFiles('./test/doublers').then(
+//   function(data) {
+//     console.log(funks.makeOutput(data));
+//   }, function(err) {
+//     console.log(err);
+//   });
