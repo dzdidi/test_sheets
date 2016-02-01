3c51eea11675491ac1cdf890341ae8be857069f4
Denys Zaliskyi <deniszalessky@gmail.com>
2016-02-01 17:38:58 +0100

    partial template commit | intermediate text commit

--- test/template.js
+++ test/template.js
@@ -68,39 +68,25 @@ describe('Test coverage for template module', function() {
     });
   });
 
-  describe('Test coverage for makeCall', function() {
-    it('should have makeCall function', function(done) {
-      assert.equal(typeof template.makeCall, 'function');
-
-      done();
-    });
-
-    it('should accept two parameters', function(done) {
-      assert.equal(template.makeCall.length, 2);
-
-      done();
-    });
-
-    it('should return call string', function(done) {
-      assert.equal(template.makeCall('script', 'arguments'), 'scrapBase.prototype.execScript(this, \'script\', arguments, ' + template.compareAndReport.toString() + ');');
-
-      done();
-    });
-  });
-
-  describe('Test coverage for compareAndReport', function() {
-    it('should have compareAndReport function', function(done) {
-      assert.equal(typeof template.compareAndReport, 'function');
-
-      done();
-    });
-
-    it('should accept two paramters', function(done) {
-      assert.equal(template.compareAndReport.length, 2);
-
-      done();
-    });
-  });
+  // describe('Test coverage for makeCall', function() {
+  //   it('should have makeCall function', function(done) {
+  //     assert.equal(typeof template.makeCall, 'function');
+  //
+  //     done();
+  //   });
+  //
+  //   it('should accept two parameters', function(done) {
+  //     assert.equal(template.makeCall.length, 2);
+  //
+  //     done();
+  //   });
+  //
+  //   it('should return call string', function(done) {
+  //     assert.equal(template.makeCall('script', 'arguments'), 'scrapBase.prototype.execScript(this, \'script\', arguments, ' + template.compare.toString() + ');');
+  //
+  //     done();
+  //   });
+  // });
 
   describe('Test coverage for getValue function', function() {
     it('should have getValue function', function(done) {
@@ -136,11 +122,11 @@ describe('Test coverage for template module', function() {
     });
 
     it('should return object with values from the provided row in a provided sheet', function(done) {
-      assert.deepEqual(template.getRow(1, testSheetObject.Sheets.Sheet1, testSheetObject.transformedScheme), { description: 'Create new bank account' });
+      assert.deepEqual(template.getRow(testSheetObject.Sheets.Sheet1, 1, testSheetObject.transformedScheme), { description: 'Create new bank account' });
 
-      assert.deepEqual(template.getRow(2, testSheetObject.Sheets.Sheet1, testSheetObject.transformedScheme), { moduleUnderTest: 'module_under_test.js' });
+      assert.deepEqual(template.getRow(testSheetObject.Sheets.Sheet1, 2, testSheetObject.transformedScheme), { moduleUnderTest: 'module_under_test.js' });
 
-      assert.deepEqual(template.getRow(3, testSheetObject.Sheets.Sheet1, testSheetObject.transformedScheme), testSheetObject.thirdRow);
+      assert.deepEqual(template.getRow(testSheetObject.Sheets.Sheet1, 3, testSheetObject.transformedScheme), testSheetObject.thirdRow);
 
       done();
     });
@@ -155,6 +141,66 @@ describe('Test coverage for template module', function() {
 
     it('should accpet one parameter', function(done) {
       assert.equal(template.translateRow.length, 1);
+
+      done();
+    });
+  });
+
+  describe('Test coverage for compare', function() {
+    it('should have compare function', function(done) {
+      assert.equal(typeof template.compare, 'function');
+
+      done();
+    });
+
+    it('should accept three paramters', function(done) {
+      assert.equal(template.compare.length, 3);
+
+      done();
+    });
+
+    it('should deeply compare scheme if last parameter is \'|\'', function(done) {
+      var obj1 = {a:1, b:{c: 2, d: {e: 3}}};
+      var obj2 = {a:4, b:{c: 5, d: {e: 6}}};
+      var obj3 = {a:4, b:{c: 5, d: {f: 6}}};
+
+      assert.equal(template.compare(obj1, obj2, '|'), true);
+      assert.equal(template.compare(obj1, obj3, '|'), false);
+
+      done();
+    });
+
+    it('should deeply objects is last parameter is \'||\'', function(done) {
+      var obj1 = {a:1, b:{c: 2, d: {e: 3}}};
+      var obj2 = {a:1, b:{c: 2, d: {e: 3}}};
+      var obj3 = {a:1, b:{c: 2, d: {f: 3}}};
+      var obj4 = {a:1, b:{c: 2, d: {e: 6}}};
+
+      assert.equal(template.compare(obj1, obj2, '||'), true);
+      assert.equal(template.compare(obj1, obj3, '||'), false);
+      assert.equal(template.compare(obj1, obj4, '||'), false);
+
+      done();
+    });
+  });
+
+  describe('Test coverage for getDeepKeys', function() {
+    it('should have getDeepKeys function', function(done) {
+      assert.equal(typeof template.getDeepKeys, 'function');
+
+      done();
+    });
+
+    it('should accept two paramters', function(done) {
+      assert.equal(template.getDeepKeys.length, 2);
+
+      done();
+    });
+
+    it('should  return deep getDeepKeys', function(done) {
+      var obj1 = {a:1, b:{c: 2, d: {e: 3}}};
+
+      assert.deepEqual(template.getDeepKeys(obj1, []), ['a', 'b', 'c', 'd', 'e']);
 
       done();
     });
